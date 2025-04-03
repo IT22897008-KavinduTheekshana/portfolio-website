33be9fb6be4ec492368dff90eea6d397e11e748c
KAVINDU <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2025-04-03 07:40:21 +0530

    add email section and footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import AboutSection from "./components/AboutSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import ProjectsSection from "./components/ProjectsSection";
@@ -9,9 +11,12 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+
         <AboutSection />
         <ProjectsSection />
+        <EmailSection />
       </div>
+      <Footer />
     </main>
   );
 }
