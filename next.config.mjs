bd68bf3630d521cc15568ef189b114685f28cee8
KAVINDU <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2025-04-10 16:02:05 +0530

    create mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
+  // Comment out for local dev
+  // basePath: "/portfolio-website",
+  // assetPrefix: "/portfolio-website/",
+  trailingSlash: true,
+};
 
 export default nextConfig;
